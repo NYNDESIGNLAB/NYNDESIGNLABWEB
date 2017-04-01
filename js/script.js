22631e73d4e7ec28e3101db8a2fba7dc4486c0aa
CeliaLRND <celia.lerandy@gmail.com>
2017-04-02 07:35:43 +0800

    v2
    lighter pics

--- js/script.js
+++ js/script.js
@@ -114,6 +114,109 @@ $('#gallery-icon-3,#gallery-link-3').on('click', function() {
 });
 
 
+
+$('#gallery-icon-5,#gallery-link-5').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj5/1.jpg',
+            
+            }, {
+            'src': './img/proj5/2.jpg',
+            
+            }, {
+            'src': './img/proj5/3.jpg',
+            
+            }, {
+            'src': './img/proj5/4.jpg',
+            
+            }, {
+            'src': './img/proj5/5.jpg',
+            
+            }, {
+            'src': './img/proj5/6.jpg',
+            
+            }, {
+            'src': './img/proj5/7.jpg',
+            
+            }, {
+            'src': './img/proj5/8.jpg',
+            
+            }, {
+            'src': './img/proj5/9.jpg',
+            
+            }, {
+            'src': './img/proj5/10.jpg',
+            
+            }]
+    })
+ 
+});
+
+$('#gallery-icon-6,#gallery-link-6').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj6/1.jpg',
+            
+            }, {
+            'src': './img/proj6/2.jpg',
+            
+            }, {
+            'src': './img/proj6/3.jpg',
+            
+            }, {
+            'src': './img/proj6/4.jpg',
+            
+            }, {
+            'src': './img/proj6/5.jpg',
+            
+            }]
+    })
+ 
+});
+
+
+$('#gallery-icon-7,#gallery-link-7').on('click', function() {
+ 
+    $(this).lightGallery({
+        dynamic: true,
+        download: false,
+        counter: false,
+        dynamicEl: [{
+            'src': './img/proj7/1.jpg',
+            
+            }, {
+            'src': './img/proj7/2.jpg',
+            
+            }, {
+            'src': './img/proj7/3.jpg',
+            
+            }, {
+            'src': './img/proj7/4.jpg',
+            
+            }, {
+            'src': './img/proj7/5.jpg',
+            
+            }, {
+            'src': './img/proj7/6.jpg',
+            
+            }, {
+            'src': './img/proj7/7.jpg',
+            
+            }]
+    })
+ 
+});
+
+
+
 });
 
 
